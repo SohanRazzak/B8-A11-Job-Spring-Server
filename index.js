f58ca7716b75a18429309fd055646011a561abf2
Sohan Razzak <>
2024-03-05 00:43:12 +0600

    sign in and up complete

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 
 // middleware
 const corsConfig = {
-    origin: ["http://localhost:5173/"],
+    origin: "http://localhost:5173",
     crendentials: true
 }
 dotenv.config();
@@ -24,16 +24,16 @@ app.use(cookieParser());
 const uri = process.env.DB_URI;
 
 
-// Uncomment to ping database
-// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-// const client = new MongoClient(uri, {
-//     serverApi: {
-//         version: ServerApiVersion.v1,
-//         strict: true,
-//         deprecationErrors: true,
-//     }
-// });
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
 
+// Uncomment to ping database
 // async function run() {
 //     try {
 //         // Connect the client to the server	(optional starting in v4.7)
@@ -49,12 +49,47 @@ const uri = process.env.DB_URI;
 // run().catch(console.dir);
 
 
+// Databases
+const database = client.db("Job_Spring");
+const userCollection = database.collection("userCollection");
+
+
 // API Endpoints
 
-    // Test Server
-    app.get('/', (req, res)=>{
-        res.send("Searching For Jobs............")
-    })
+// Get
+
+// Post
+app.post("/users", async (req, res) => {
+    try {
+        const user = req.body;
+        const result = await userCollection.insertOne(user);
+        res.send(result)
+    }
+    catch (error) {
+        console.log(error);
+    }
+})
+
+// Put
+app.put("/users", async(req, res)=>{
+    const user = req.body;
+    const options = { upsert: true };
+    const filter = { email: user.email }
+    const updatedUser = {
+        $set : {
+            ...user
+        }
+    }
+    const result = await userCollection.updateOne(filter, updatedUser, options);
+    res.send(result)
+})
+
+
+
+// Test Server
+app.get('/', (req, res) => {
+    res.send("Searching For Jobs............")
+})
 
 
 
@@ -64,6 +99,6 @@ const uri = process.env.DB_URI;
 
 
 // Listen to port
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`Running on port no: ${port}`)
 })
