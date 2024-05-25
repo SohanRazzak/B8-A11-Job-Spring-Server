f65828080e7ca8212e2476d25971fdbdb376e7c4
Sohan Razzak <>
2024-05-25 22:12:51 +0600

    apply for jobs route setuped

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 5000;
 
 // middleware
 const corsConfiguration = {
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://b8-a11-job-spring-server.onrender.com"],
     credentials: true
 }
 dotenv.config();
